0b1b357f28a8ad1556b730ada7ae338e85361be2
Josh Elkind <75272482+JoshElkind@users.noreply.github.com>
2021-08-28 19:57:50 -0700

    Script To Run On Reboot
    This script is meant to be run on the boot of the pc. This script first frees the camera for use and then runs the Main.py script.

---
+++ run_on_reboot_Pi4_HomeMonitor.sh
@@ -0,0 +1,8 @@
+
+#nohup (sudo python3 /home/pi/VSCode/Python/FaceRecogRasPi/Main.py) &
+sudo systemctl stop motion
+sudo system stop motion
+sudo python3 /home/pi/VSCode/Python/FaceRecogRasPi/Main.py &
+#sudo touch /home/test.txt
+
+
