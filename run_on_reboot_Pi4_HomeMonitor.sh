56c43bc6e251990622854cf31ec0a6e4fb08fbcb
Josh Elkind <75272482+JoshElkind@users.noreply.github.com>
2021-09-05 20:17:59 -0400

    Update 1: stream capability
    Added reboot status to the server listen scripts.

--- run_on_reboot_Pi4_HomeMonitor.sh
+++ run_on_reboot_Pi4_HomeMonitor.sh
@@ -1,8 +1,13 @@
-
+xhost +SI:localuser:root
+export DISPLAY=0:0
 #nohup (sudo python3 /home/pi/VSCode/Python/FaceRecogRasPi/Main.py) &
 sudo systemctl stop motion
-sudo system stop motion
-sudo python3 /home/pi/VSCode/Python/FaceRecogRasPi/Main.py &
-#sudo touch /home/test.txt
-
-
+sudo system motion stop
+sudo modprobe v4l2loopback exclusive_caps=1 video_nr=2
+sudo touch ffmep.txt
+sudo touch s8test.txt
+sudo ffmpeg -i /dev/video0 -f v4l2 -codec:v rawvideo -pix_fmt yuv420p /dev/vide$
+sudo python3 /home/pi/VSCode/Python/stream8081_pl.py & >> /s8test.txt 2>&1 
+sudo echo "--1--" >> /test_sh.txt
+sudo python3 /home/pi/VSCode/Python/FaceRecogRasPi/Main1.py >> /err_stan_file.t$
+sudo echo "--3--" >> /test_sh.txt
